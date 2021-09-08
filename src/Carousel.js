f010f15e016e56eb40845ef969bf6d5c078fc152
kirilsvoloshins <kv.voloshin@gmail.com>
2021-09-08 05:57:05 +0300

    some adjustments

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,6 +2,53 @@ import React from "react";
 import "./Carousel.css";
 import { useState, useEffect, useRef } from "react";
 
+/* External helper functions */
+async function wait() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), 1)
+    });
+}
+const getCleanSlideData = (slides, slideId) => `<div>${typeof slides[slideId] === "undefined" ? "" : slides[slideId]}</div>`; /* will not show "undefined" instead of non-existing slides */
+const getUserSwipeDirection = (newMarginLeft) => ({ isUserSwipingRight: newMarginLeft < -100, isUserSwipingLeft: newMarginLeft >= -100 });
+const wasSwipeTooFast = (e, swipeStartData) => {
+    const MIN_SWIPE_DURATION_MS = 100;
+    const swipeDuration = e.timeStamp - swipeStartData.current.timeStamp;
+    /* if (swipeDuration <= MIN_SWIPE_DURATION_MS) console.error(`SWIPE TOO FAST at (${swipeDuration}) ms.`); */
+    return swipeDuration <= MIN_SWIPE_DURATION_MS;
+};
+const wasSwipeTooShort = (e, swipeStartData) => {
+    const MIN_SWIPE_DISTANCE_PX = 50;
+    const currentSwipeX = e.changedTouches[0].clientX;
+    const swipeStartScreenX = swipeStartData.current.changedTouches[0].clientX;
+    const swipeDeltaX = currentSwipeX - swipeStartScreenX;
+    /* if (Math.abs(swipeDeltaX) < MIN_SWIPE_DISTANCE_PX) console.error(`SWIPE TOO SHORT at (${swipeDeltaX}) px.`) */
+    return Math.abs(swipeDeltaX) < MIN_SWIPE_DISTANCE_PX;
+};
+const isThereNoSlideThere = (isUserSwipingRight, currentlySelectedSlideId) => {
+    const idOfSlideToCheckExisting = isUserSwipingRight ? currentlySelectedSlideId + 1 : currentlySelectedSlideId - 1;
+    const closestPossibleSlideIndex = returnClosestPossibleSlideIndex(idOfSlideToCheckExisting);
+    return idOfSlideToCheckExisting !== closestPossibleSlideIndex;
+}; /* just checking if the slide the user is about to open exists. */
+const getSlidesDataHTML = (slides, slideId) => [getCleanSlideData(slides, slideId - 1), getCleanSlideData(slides, slideId), getCleanSlideData(slides, slideId + 1)].join("");
+const getSlidesDataHTML_duplicate12 = (slides, slideId) => [getCleanSlideData(slides, slideId), getCleanSlideData(slides, slideId), getCleanSlideData(slides, slideId + 1)].join(""); /* animating slides the "easy" way turned out to be quite complex since I have chosen to store simultaneously only three slides and animations are done by changing marginLeft. The way it works is there are three slides on screen simultaneously. The user sees only the middle one - previous and next ones are hidden. Once the user swipes to the next slide, the previous and current are hidden. Since there are only three slides and I kinda need to reset the margin for selected slide to be the middle one (so the user can continue switching between slides), I duplicate the content of the slide being shown to the middle slide and change marginLeft back for the middle slide to be shown. Then I update the content of all slides to store newPrevious, newCurrent and newNext slide actual content.*/
+const getSlidesDataHTML_duplicate23 = (slides, slideId) => [getCleanSlideData(slides, slideId), getCleanSlideData(slides, slideId + 1), getCleanSlideData(slides, slideId + 1)].join("");
+const changeSlideContent = (slides, slideId, contentContainer) => contentContainer.current.innerHTML = getSlidesDataHTML(slides, slideId);
+const changeSlideContentDuplicateVisibleSlideToNextOne = (slides, slideId, contentContainer) => contentContainer.current.innerHTML = getSlidesDataHTML_duplicate12(slides, slideId);
+const changeSlideContentDuplicateVisibleSlideToPreviousOne = (slides, slideId, contentContainer) => contentContainer.current.innerHTML = getSlidesDataHTML_duplicate23(slides, slideId);
+const returnClosestPossibleSlideIndex = (passedValue, firstSlideId, lastSlideId) => {
+    if (passedValue < firstSlideId) return firstSlideId;
+    if (passedValue > lastSlideId) return lastSlideId;
+    return passedValue;
+}; /* sometimes the user can try to open a non-existing slide. Let's prevent that. */
+const returnRecommendedSlideIndex = (humanValue, firstSlideId, lastSlideId) => {
+    if (isNaN(humanValue)) return firstSlideId;
+    const properSlideIndex = humanValue - 1;
+    return returnClosestPossibleSlideIndex(properSlideIndex, firstSlideId, lastSlideId);
+}; /* sometimes the user can try to open a non-existing slide. Let's prevent that. This function handles user-input value. */
+const handleSlideIdInputKeyboardPressEvent = ({ keyCode }) => keyCode === 13 ? handleSlideNumberManualInput() : '';
+
+
+
 export function Carousel(props) {
     const { slides } = props;
     const firstSlideId = 0, amountOfSlides = slides.length, lastSlideId = amountOfSlides - 1;
@@ -21,55 +68,11 @@ export function Carousel(props) {
     useEffect(() => {
         slideIdInputField.current.value = currentlySelectedSlideId + 1; /* change the number of slide being shown. */
         manageScrollButtonVisibility(currentlySelectedSlideId); /* hide scroll buttons on first and last slides. */
-        changeSlideContent(currentlySelectedSlideId); /* return slide container to the initial state. */
+        changeSlideContent(slides, currentlySelectedSlideId, contentContainer); /* return slide container to the initial state. */
     }, [currentlySelectedSlideId]);
 
-    /* SMALL HELPER FUNCTIONS */
-    const getCleanSlideData = slideId => `<div>${typeof slides[slideId] === "undefined" ? "" : slides[slideId]}</div>`; /* will not show "undefined" instead of non-existing slides */
-    const getUserSwipeDirection = () => ({ isUserSwipingRight: newMarginLeft < -100, isUserSwipingLeft: newMarginLeft >= -100 });
-    const wasSwipeTooFast = ({ timeStamp }) => {
-        const MIN_SWIPE_DURATION_MS = 100;
-        const swipeDuration = timeStamp - swipeStartData.current.timeStamp;
-        /* if (swipeDuration <= MIN_SWIPE_DURATION_MS) console.error(`SWIPE TOO FAST at (${swipeDuration}) ms.`); */
-        return swipeDuration <= MIN_SWIPE_DURATION_MS;
-    };
-    const wasSwipeTooShort = (e) => {
-        const MIN_SWIPE_DISTANCE_PX = 50;
-        const currentSwipeX = e.changedTouches[0].clientX;
-        const swipeStartScreenX = swipeStartData.current.changedTouches[0].clientX;
-        const swipeDeltaX = currentSwipeX - swipeStartScreenX;
-        /* if (Math.abs(swipeDeltaX) < MIN_SWIPE_DISTANCE_PX) console.error(`SWIPE TOO SHORT at (${swipeDeltaX}) px.`) */
-        return Math.abs(swipeDeltaX) < MIN_SWIPE_DISTANCE_PX;
-    };
-    const isThereNoSlideThere = (isUserSwipingRight) => {
-        const idOfSlideToCheckExisting = isUserSwipingRight ? currentlySelectedSlideId + 1 : currentlySelectedSlideId - 1;
-        const closestPossibleSlideIndex = returnClosestPossibleSlideIndex(idOfSlideToCheckExisting);
-        return idOfSlideToCheckExisting !== closestPossibleSlideIndex;
-    }; /* just checking if the slide the user is about to open exists. */
-    const getSlidesDataHTML = slideId => [getCleanSlideData(slideId - 1), getCleanSlideData(slideId), getCleanSlideData(slideId + 1)].join("");
-    const getSlidesDataHTML_duplicate12 = slideId => [getCleanSlideData(slideId), getCleanSlideData(slideId), getCleanSlideData(slideId + 1)].join(""); /* animating slides the "easy" way turned out to be quite complex since I have chosen to store simultaneously only three slides and animations are done by changing marginLeft. The way it works is there are three slides on screen simultaneously. The user sees only the middle one - previous and next ones are hidden. Once the user swipes to the next slide, the previous and current are hidden. Since there are only three slides and I kinda need to reset the margin for selected slide to be the middle one (so the user can continue switching between slides), I duplicate the content of the slide being shown to the middle slide and change marginLeft back for the middle slide to be shown. Then I update the content of all slides to store newPrevious, newCurrent and newNext slide actual content.*/
-    const getSlidesDataHTML_duplicate23 = slideId => [getCleanSlideData(slideId), getCleanSlideData(slideId + 1), getCleanSlideData(slideId + 1)].join("");
-    const changeSlideContent = slideId => contentContainer.current.innerHTML = getSlidesDataHTML(slideId);
-    const changeSlideContentDuplicateVisibleSlideToNextOne = slideId => contentContainer.current.innerHTML = getSlidesDataHTML_duplicate12(slideId);
-    const changeSlideContentDuplicateVisibleSlideToPreviousOne = slideId => contentContainer.current.innerHTML = getSlidesDataHTML_duplicate23(slideId);
-    const returnClosestPossibleSlideIndex = passedValue => {
-        if (passedValue < firstSlideId) return firstSlideId;
-        if (passedValue > lastSlideId) return lastSlideId;
-        return passedValue;
-    }; /* sometimes the user can try to open a non-existing slide. Let's prevent that. */
-    const returnRecommendedSlideIndex = humanValue => {
-        if (isNaN(humanValue)) return firstSlideId;
-        const properSlideIndex = humanValue - 1;
-        return returnClosestPossibleSlideIndex(properSlideIndex);
-    }; /* sometimes the user can try to open a non-existing slide. Let's prevent that. This function handles user-input value. */
-    const handleSlideIdInputKeyboardPressEvent = ({ keyCode }) => keyCode === 13 ? handleSlideNumberManualInput() : '';
 
     /* MAIN FUNCTIONS */
-    async function wait() {
-        return new Promise(resolve => {
-            setTimeout(() => resolve(), 1)
-        });
-    }
     async function animateLeft() {
         isAnimationStillActive.current = true;
         return new Promise(async (resolve) => {
@@ -83,7 +86,7 @@ export function Carousel(props) {
                 }
             }
         });
-    }; /* opening the previous slide is actually a for loop with setTimeouts to "animate". */
+    };
     async function animateRight() {
         isAnimationStillActive.current = true;
         return new Promise(async (resolve) => {
@@ -97,21 +100,21 @@ export function Carousel(props) {
                 }
             }
         });
-    }; /* opening the next slide is actually a for loop with setTimeouts to "animate". */
+    };
     async function animateWhatsLeftToGoToPreviousSlide() {
         await animateLeft();
         const newCurrentlySelectedSlideId = currentlySelectedSlideId - 1;
-        changeSlideContentDuplicateVisibleSlideToNextOne(newCurrentlySelectedSlideId);
+        changeSlideContentDuplicateVisibleSlideToNextOne(slides, newCurrentlySelectedSlideId, contentContainer);
         setMarginLeft(-100);
         selectSlideById(newCurrentlySelectedSlideId);
-    }; /* this fn is kinda universal since it can be used for switching slides both from buttons and swipes. */
+    };
     async function animateWhatsLeftToGoToNextSlide() {
         await animateRight();
         const newCurrentlySelectedSlideId = currentlySelectedSlideId + 1;
-        changeSlideContentDuplicateVisibleSlideToPreviousOne(newCurrentlySelectedSlideId);
+        changeSlideContentDuplicateVisibleSlideToPreviousOne(slides, newCurrentlySelectedSlideId, contentContainer);
         setMarginLeft(-100);
         selectSlideById(newCurrentlySelectedSlideId);
-    }; /* this fn is kinda universal since it can be used for switching slides both from buttons and swipes. */
+    };
     const changeToPreviousSlide = () => (currentlySelectedSlideId !== firstSlideId && isAnimationStillActive.current !== true) ? animateWhatsLeftToGoToPreviousSlide() : '';/* can't change to a non-existing slide. */
     const changeToNextSlide = () => (currentlySelectedSlideId !== lastSlideId && isAnimationStillActive.current !== true) ? animateWhatsLeftToGoToNextSlide() : ''; /* can't change to a non-existing slide. */
     function manageScrollButtonVisibility(slideId) {
@@ -120,7 +123,7 @@ export function Carousel(props) {
     }; /* let's hide scroll buttons on the first and last slides */
     function handleSlideNumberManualInput() {
         const valueBeingInput = parseInt(slideIdInputField.current.value);
-        const properSlideIndex = returnRecommendedSlideIndex(valueBeingInput);
+        const properSlideIndex = returnRecommendedSlideIndex(valueBeingInput, firstSlideId, lastSlideId);
         selectSlideById(properSlideIndex);
         slideIdInputField.current.value = currentlySelectedSlideId + 1;
     }; /* check data once the user clicks the "go" button to open the slide which id has been input. */
@@ -137,7 +140,7 @@ export function Carousel(props) {
     }; /* finger-following swipes */
     function handleSwipeEnd(e) {
         const { isUserSwipingRight, isUserSwipingLeft } = getUserSwipeDirection();
-        if (wasSwipeTooFast(e) || wasSwipeTooShort(e) || isThereNoSlideThere(isUserSwipingRight)) {
+        if (wasSwipeTooFast(e, swipeStartData) || wasSwipeTooShort(e, swipeStartData) || isThereNoSlideThere(isUserSwipingRight, currentlySelectedSlideId)) {
             setMarginLeft(-100);
             return swipeStartData.current = null;
         }
